c2df295b8d77f017f826ff5073b780be9a9d664b
Patrick Coxall <mr.coxall@mths.ca>
2020-01-08 23:07:39 -0500

    Update code.py

--- docs/sprites/code/code.py
+++ docs/sprites/code/code.py
@@ -18,11 +18,16 @@ def game_scene():
     # sets the background to image 0 in the bank
     background = stage.Grid(image_bank_1, 160, 120)
 
+    # the ship sprite(the 5th image, starting to count at 0) 
+    #   will show up at location (80,60)
+    ship = stage.Sprite(image_bank_1, 5, 80, 60)
+    sprites.append(ship)
+
     # create a stage for the background to show up on
     #   and set the frame rate to 60fps
     game = stage.Stage(ugame.display, 60)
-    # set the layers, items show up in order
-    game.layers = [background]
+    # set the layers, items show up in order, ship and background behind
+    game.layers = [ship] + [background]
     # render the background and inital location of sprite list
     # most likely you will only render background once per scene
     game.render_block()
